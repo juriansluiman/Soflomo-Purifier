2f08dad98013e471bdcae7e5526ddf11f883697e
Stefano Torresi <stefano@torresi.io>
2016-03-10 22:00:58 +0100

    bump next mini dev version

--- src/Module.php
+++ src/Module.php
@@ -9,7 +9,7 @@ use Zend\ModuleManager\Feature;
 
 class Module implements Feature\ConfigProviderInterface
 {
-    const VERSION = '1.0.2';
+    const VERSION = '1.0.3-dev';
 
     public function getConfig()
     {
